bf665c53fa2bf55343e7b100b19832e42caaae55
Tibor Simon <tibor@tiborsimon.io>
2016-03-02 23:13:31 +0100

    Small changes and image added

--- MathJaxTest01/ViewController.swift
+++ MathJaxTest01/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
         let path = NSBundle.mainBundle().bundlePath
         let baseURL = NSURL.fileURLWithPath(path)
         
-        let myHTML = "<!DOCTYPE html><html><head><title>MathJax TeX Test Page</title><script type=\"text/javascript\" async  src=\"MathJax-2.6.1/MathJax.js?config=TeX-AMS_CHTML\"></script></head><body>  <div style=\"font-size: 30pt; color: #eee;\">    $$    \\begin{equation}      \\prod_{\\substack{                1\\le i \\le n\\\\                1\\le j \\le m}}         M_{i,j}    \\end{equation}$$  </div></body></html>"
+        let myHTML = "<!DOCTYPE html><html><head><title>MathJax TeX Test Page</title><script type=\"text/javascript\" async  src=\"MathJax-2.6.1/MathJax.js?config=TeX-AMS_CHTML\"></script></head><body>  <div style=\"font-size: 30pt; color: #eee;\">$$\\begin{equation}\\prod_{\\substack{1\\le i \\le n\\\\ 1\\le j \\le m}}M_{i,j} \\end{equation}$$</div></body></html>"
         myWebView.loadHTMLString(myHTML, baseURL:baseURL)
         
     }
